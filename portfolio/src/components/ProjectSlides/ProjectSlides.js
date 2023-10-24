d76ee6d8749fdafbafcbd38b89af573311342d0c
Akshat Shrivastava <akshatrules04@gmail.com>
2023-10-24 17:40:46 -0400

    Create ProjectSlides template.

--- portfolio/src/components/ProjectSlides/ProjectSlides.js
+++ portfolio/src/components/ProjectSlides/ProjectSlides.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {Button} from '@mui/material';
 
 const ProjectSlides = () => {
+  const items = ['Project Name','Project Description','Tech Stack'];
+  const[curIndex, setCurIndex] = useState(0);
+  const goToPrevSlide = () => {
+    setCurIndex((prevIndex)=> {
+      if(prevIndex===0){
+        return items.length-1;
+      } else{
+        return prevIndex-1;
+      }
+    });
+  };
+  const goToNextSlide = () => {
+    setCurIndex((prevIndex)=> {
+      if(prevIndex===items.length-1){
+        return 0;
+      } else{
+        return prevIndex+1;
+      }
+    });
+  };
   return (
-    <div>ProjectSlides</div>
+    <div>
+      <h1>ProjectSlides</h1>
+      {curIndex!==0 &&(<Button onClick={goToPrevSlide}>Prev</Button>)}
+      {curIndex!== items.length-1 && (<Button onClick={goToNextSlide}>Next</Button>)}
+      <h2>{items[curIndex]}</h2>
+    </div>
+    
   )
 }
 
